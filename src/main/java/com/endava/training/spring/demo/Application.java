796d76b6439af77477833b9b967181487f708744
Vitalie Safronovici <vitalie.safronovici@endava.com>
2019-03-05 15:01:04 +0200

    scoping

--- src/main/java/com/endava/training/spring/demo/Application.java
+++ src/main/java/com/endava/training/spring/demo/Application.java
@@ -1,7 +1,9 @@
 package com.endava.training.spring.demo;
 
+import com.endava.training.spring.demo.entity.Person;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -22,10 +24,27 @@ public class Application {
         SpringApplication.run(Application.class);
     }
 
+    @Autowired
+    private Person person1;
+
+    @Autowired
+    private Person person2;
+
+    @Autowired
+    private Person person3;
+
+    @Autowired
+    private Person person4;
+
+
+
     @Bean
     public CommandLineRunner hello() {
         return (args) -> {
-            LOG.info("==== Hello world !!!");
+            System.out.println("==== p1:" + person1);
+            System.out.println("==== p2:" + person2);
+            System.out.println("==== p3:" + person3);
+            System.out.println("==== p4:" + person4);
         };
     }
 
